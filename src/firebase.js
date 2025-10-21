fd9657e24d2e7de9935059fe61e9c49c39ec5de7
yutakata04 <yutakata04@yutakata04noMacBook-Air.local>
2025-10-22 01:28:54 +0900

    Fix Firebase config for production environment

--- src/firebase.js
+++ src/firebase.js
@@ -5,9 +5,9 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDdULG2JiqKTIkF1AvcZeJse8vQHVzijnA",
-  authDomain: "mahjong-first.firebaseapp.com",
-  projectId: "mahjong-first",
-  storageBucket: "mahjong-first.firebasestorage.app",
+  authDomain: "mahjong-dev-a4d01.firebaseapp.com",
+  projectId: "mahjong-dev-a4d01",
+  storageBucket: "mahjong-dev-a4d01.firebasestorage.app",
   messagingSenderId: "296803295236",
   appId: "1:296803295236:web:4f7090147a36bdbccb9551",
   measurementId: "G-HJ3H6Z35DZ"
